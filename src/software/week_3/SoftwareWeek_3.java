0db54378b5240d24a0139710339b56e0bbe4a242
soni-z <soniz@sheridancollege.ca>
2022-01-24 09:04:36 -0500

    version 1

---
+++ src/software/week_3/SoftwareWeek_3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package software.week_3;
+
+/**
+ *
+ * @author zeelsoni
+ */
+public class SoftwareWeek_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("learning github");
+    }
+    
+}
